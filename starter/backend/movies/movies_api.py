24ac3643e0f85422776b496701c722901a766b86
carlos <wuopcarlos@gmail.com>
2024-12-19 15:18:17 +0000

    Test Backend CI Trigger 2

--- starter/backend/movies/movies_api.py
+++ starter/backend/movies/movies_api.py
@@ -9,3 +9,4 @@ movies_api.add_url_rule("/movies", view_func=movies, methods=["POST"])
 movies_api.add_url_rule("/movies/<int:movie_id>", view_func=movies, methods=["GET", "PUT", "DELETE"])
 # Test Backend CI Trigger
 # Test Backend CI Trigger
+# Test Backend CI Trigger 2
